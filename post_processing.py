bd282349527948e4bc6fd6aa8686acf2d9ee5589
nonresidentaliennico <143525682+nonresidentaliennico@users.noreply.github.com>
2024-05-11 16:37:45 +0200

    .

--- post_processing.py
+++ post_processing.py
@@ -1,9 +1,9 @@
-from qsm_power import main
+from qsm import main
 import os 
 from datetime import datetime as dt
 
 
-simulations_folder_path = '/Users/nico/Documents/school/thesis/wing_motion_python/single-wing-simulations/diptera'
+simulations_folder_path = '/home/nico/Documents/school/thesis/wing_motion_python/single-wing-simulations/'
 
 insects_simulations = os.listdir(simulations_folder_path)
 
@@ -18,7 +18,7 @@ def make_dir(dir_name, index=0):
         # print(split)
         new = '/'.join(split[:-1])
         make_dir(new, index=index+1) 
-    new_dir = '/Users/nico/Documents/GitHub/QSM-insects/'+dir_name
+    new_dir = '/home/nico/Documents/GitHub/QSM-insects/'+dir_name
     if not os.path.exists(new_dir):
         os.mkdir(new_dir)
     return new_dir
@@ -37,7 +37,7 @@ def writeArraytoCSV(header, array, file_name):
         file.write('\n')
 
 for insect_simulation in insects_simulations:
-    if insect_simulation == 'hoverfly-simulation': #this one can be commented out. it's currently the test folder but if commented out, all folders will be parsed. 
+    if insect_simulation == 'musca-simulations': #this one can be commented out. it's currently the test folder but if commented out, all folders will be parsed. 
         insect_simulation_path = os.path.join(simulations_folder_path, insect_simulation)
         case_studies = os.listdir(insect_simulation_path)
         for case_study in case_studies:
@@ -50,10 +50,13 @@ for insect_simulation in insects_simulations:
                 # exit()
                 for cfd_run in cfd_runs:
                     cfd_run_path = os.path.join(simulations_folder_path, insect_simulation, case_study, cfd_run)
+                    # print(cfd_run_path)
+                    # exit()
                     if os.path.isdir(cfd_run_path):
                         folder_name = f'post-processing/{insect_simulation}/{case_study}/{cfd_run}/{rightnow}'
                         make_dir(folder_name)
                         forces_coefficients, moment_coefficient, power_coefficient = main(cfd_run_path, folder_name)
                         writeArraytoCSV(['Cl1', 'Cl2', 'Cd1', 'Cd2', 'Crot', 'Cam1', 'Cam2', 'Crd', 'K0_forces'], forces_coefficients, folder_name+'/forces_coefficients.csv')
-                        writeArraytoCSV(['C_lever (optimized lever)', 'Lever (non-optimized lever)', 'K0_moment'], moment_coefficient, folder_name+'/moment_coefficient.csv')
-                        writeArraytoCSV(['C_power', 'K0_power'],  power_coefficient, folder_name+'/power_coefficient.csv')
+                        writeArraytoCSV(['C_lever_x_w (lever x-component optimized for moments)', 'Lever (lever y-component optimized for moments)', 'K0_moments'], moment_coefficient, folder_name+'/moment_coefficient.csv')
+                        writeArraytoCSV(['C_lever_x_w (lever x-component optimized for power)', 'Lever (lever y-component optimized for power)', 'K0_power'],  power_coefficient, folder_name+'/power_coefficient.csv')
+                        exit()
